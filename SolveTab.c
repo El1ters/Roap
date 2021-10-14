e06a1d59ebcb9a51d3d17cd1982d404aab8506cc
Miguel Lopes <82033110+El1ters@users.noreply.github.com>
2021-10-14 21:20:13 +0100

    Add files via upload

--- SolveTab.c
+++ SolveTab.c
@@ -40,68 +40,98 @@ void SolveTab(int **tabuleiro,char *mode,int *def,int *dim,FILE *fp1){
     if(strcmp(mode,"A1") == 0){
         fprintf(fp1,"%d\n\n",tabuleiro[def[0] - 1][def[1] - 1]);
     }
+
     if(strcmp(mode,"A2") == 0){
-        int linha,coluna;
-        for(linha = def[0] - 2; linha <= def[0]; linha++){
-            for(coluna = def[1] - 2 ; coluna <= def[1]; coluna++){ 
-                if((linha == (def[0] - 1)) && (coluna == (def[1] - 1))){
-                    continue;
-                }
-                      
-                if(linha < 0 || coluna < 0 || (linha > dim[0]) || (coluna > dim[1])){
-                    continue;
-                }
-                
-                if(tabuleiro[linha][coluna] == 0){
-                    fprintf(fp1,"1\n\n");
-                    return;
-                }
+        if(def[0] - 2 >= 0){
+            if(tabuleiro[def[0] - 2][def[1] - 1] == 0){
+                fprintf(fp1,"1\n\n");
+                return;
             }
         }
+        if(def[0] + 1 <= dim[0]){
+            if(tabuleiro[def[0]][def[1] - 1] == 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] - 2 >= 0){
+            if(tabuleiro[def[0] - 1][def[1] - 2] == 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] + 1 <= dim[1]){
+            if(tabuleiro[def[0] - 1][def[1]] == 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }  
+        }
+            
         fprintf(fp1,"0\n\n");
         return;
     }
+
     if(strcmp(mode,"A3") == 0){
-        int linha,coluna;
-        for(linha = def[0] - 2; linha <= def[0]; linha++){
-            for(coluna = def[1] - 2 ; coluna <= def[1]; coluna++){ 
-                if((linha == (def[0] - 1)) && (coluna == (def[1] - 1))){
-                    continue;
-                }
-                      
-                if(linha < 0 || coluna < 0 || (linha > dim[0]) || (coluna > dim[1])){
-                    continue;
-                }
-                
-                if(tabuleiro[linha][coluna] > 0){
-                    fprintf(fp1,"1\n\n");
-                    return;
-                }
+         if(def[0] - 2 >= 0){
+            if(tabuleiro[def[0] - 2][def[1] - 1] > 0){
+                fprintf(fp1,"1\n\n");
+                return;
             }
+        }
+        if(def[0] + 1 <= dim[0]){
+            if(tabuleiro[def[0]][def[1] - 1] > 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] - 2 >= 0){
+            if(tabuleiro[def[0] - 1][def[1] - 2] > 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] + 1 <= dim[1]){
+            if(tabuleiro[def[0] - 1][def[1]] > 0){
+                fprintf(fp1,"1\n\n");
+                return;
+            }  
         }
         fprintf(fp1,"0\n\n");
         return;
     }
+
     if(strcmp(mode,"A4") == 0){
-        int linha,coluna;
-        for(linha = def[0] - 2; linha <= def[0]; linha++){
-            for(coluna = def[1] - 2 ; coluna <= def[1]; coluna++){ 
-                if((linha == (def[0] - 1)) && (coluna == (def[1] - 1))){
-                    continue;
-                }
-                      
-                if(linha < 0 || coluna < 0 || (linha > dim[0]) || (coluna > dim[1])){
-                    continue;
-                }
-                
-                if(tabuleiro[linha][coluna] == -1){
-                    fprintf(fp1,"1\n\n");
-                    return;
-                }
+         if(def[0] - 2 >= 0){
+            if(tabuleiro[def[0] - 2][def[1] - 1] == -1){
+                fprintf(fp1,"1\n\n");
+                return;
             }
+        }
+        if(def[0] + 1 <= dim[0]){
+            if(tabuleiro[def[0]][def[1] - 1] == -1){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] - 2 >= 0){
+            if(tabuleiro[def[0] - 1][def[1] - 2] == -1){
+                fprintf(fp1,"1\n\n");
+                return;
+            }
+        }
+        if(def[1] + 1 <= dim[1]){
+            if(tabuleiro[def[0] - 1][def[1]] == -1){
+                fprintf(fp1,"1\n\n");
+                return;
+            }  
         }
         fprintf(fp1,"0\n\n");
         return;
+    }
+    if(strcmp(mode,"A5") == 0){
+        if(tabuleiro[def[0] - 1][def[1] - 1 == 0 || tabuleiro[def[0] - 1]][def[1] - 1] == -1){
+            fprintf(fp1,"-1\n\n");
+        }
     }
 
 }
